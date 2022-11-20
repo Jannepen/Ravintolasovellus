17d5685cdd7dad14d9c263b7bae48a52ccb51c14
Janne <janne.penttala@helsinki.fi>
2022-11-20 20:06:05 +0200

    Option to add new restaurants

--- routes.py
+++ routes.py
@@ -6,10 +6,21 @@ from db import db
 def index():
     return render_template("welcome.html")
 
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.form["name"]
+    sql = "INSERT INTO restaurants (name) VALUES (:name)"
+    result = db.session.execute(sql, {"name":name})
+    db.session.commit()
+    return redirect("/")
+
 @app.route("/restaurants")
 def restaurants():
     sql = "SELECT name FROM restaurants"
     result = db.session.execute(sql)
     return render_template("restaurants.html", restaurants=result)
-
 
